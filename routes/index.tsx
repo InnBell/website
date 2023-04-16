136e7257cf336288546d96dd9d8e7e1d6c43e8d0
Siddhant Gupta <siddhant.c.gupta@accenture.com>
2023-04-16 16:15:08 +0300

    Add favicon

--- routes/index.tsx
+++ routes/index.tsx
@@ -7,6 +7,39 @@ export default function Home() {
       <Head>
         <title>InnBell</title>
         <meta />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/favicon/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/favicon/site.webmanifest" />
+        <link
+          rel="mask-icon"
+          href="/favicon/safari-pinned-tab.svg"
+          // @ts-expect-error
+          color="#7e2a00"
+        />
+        <link rel="shortcut icon" href="/favicon/favicon.ico" />
+        <meta name="apple-mobile-web-app-title" content="InnBell" />
+        <meta name="application-name" content="InnBell" />
+        <meta name="msapplication-TileColor" content="#7e2a00" />
+        <meta
+          name="msapplication-config"
+          content="/favicon/browserconfig.xml"
+        />
+        <meta name="theme-color" content="#ffffff" />
       </Head>
       <body class="font-poppins leading-normal">
         <Hero />
